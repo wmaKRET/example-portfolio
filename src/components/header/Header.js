9a276845a3946fa410534bd2c9a119578ff5086e
wmaKRET <wkret91.2@gmail.com>
2022-07-14 10:40:54 +0200

    create header structure and header components for readability

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,23 @@
 import React from "react"
 
+import CTA from "./CTA"
+import HeaderSocials from "./HeaderSocials"
+import ME from "../../assets/me.png"
 import "./header.css"
 
 function Header(){
     return (
-        <h1>header</h1>
+        <header className="container header__container">
+            <h5>Hello! My name is</h5>
+            <h1>Wojciech</h1>
+            <h5 className="text-light">Frontend Developer</h5>
+            <CTA />
+            <HeaderSocials />
+            <div className="me">
+                <img src={ME} alt="picture of me"></img>
+            </div>
+            <a href="#contact" className="scroll__down">Scroll Down</a>
+        </header>
     )
 }
 
