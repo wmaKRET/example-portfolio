f05f8db5904073bd738a0f6f7c4ee8123d6aca2e
wmaKRET <wkret91.2@gmail.com>
2022-07-15 09:52:49 +0200

    reduce project by few unnecesarry components

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,7 +3,6 @@ import React, { useState } from "react"
 import "./nav.css"
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai"
 import { BiBook, BiMessageDetail } from "react-icons/bi"
-import { RiServiceLine } from "react-icons/ri"
 
 function Nav(){
     const [activeNav, setActiveNav] = useState('#')
@@ -34,13 +33,6 @@ function Nav(){
                 onClick={() => handleNavChange('#experience')}
             >
                 <BiBook />
-            </a>
-            <a 
-                href="#services" 
-                className={activeNav === '#services' ? 'active' : ''}
-                onClick={() => handleNavChange('#services')}
-            >
-                <RiServiceLine />
             </a>
             <a 
                 href="#contact" 
