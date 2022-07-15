38cbbff42bc3b971fc053568a2f43a1a51bcfb3a
wmaKRET <wkret91.2@gmail.com>
2022-07-15 10:10:09 +0200

    create and style Contact componenet

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Nav from "./components/nav/Nav"
 import About from "./components/about/About"
 import Experience from "./components/experience/Experience"
 import Contact from "./components/contact/Contact"
-import Footer from "./components/footer/Footer"
 
 function App(){
     return (
@@ -15,7 +14,6 @@ function App(){
             <About />
             <Experience />
             <Contact />
-            <Footer />
         </>
     )
 }
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,28 @@
 import React from "react"
 
 import "./contact.css"
+import { MdOutlineEmail } from "react-icons/md"
 
-function Contact(){
+function Contact() {
     return (
-        <section id="contact">contact</section>
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className="container contact__container">
+                <div className="contact__email">
+                    <MdOutlineEmail />
+                    <h4>Email</h4>
+                    <h5>wkret91.2@gmail.com</h5>
+                    <a href="mailto:wkret91.2@gmail.com">Send Message</a>
+                </div>
+                <form className="contact__form">
+                    <input type="text" name="name" placeholder="Name..."></input>
+                    <input type="email" name="email" placeholder="Email..." required></input>
+                    <textarea name="message" placeholder="Your message..." required />
+                    <button type="submit" className="btn btn-primary">Send Message</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
